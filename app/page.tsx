8dae6c1085fccd9f15f4fd27ddd60ff972c9d472
Favy-source <71841080+Favy-source@users.noreply.github.com>
2024-01-25 21:10:51 -0800

    title changed

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ const HomePage = () => {
     <div className="h-[90vh]  flex flex-col justify-between"> 
       <div className="flex flex-col gap-3 items-center justify-center mt-7 ">
         <h3 className="font-semibold lg:text-4xl text-3xl text-center">
-          <span className="text-purple-900 ">ELH</span> Empowers And
-          <br /> Connects Minds
+          <span className="text-purple-900 ">ELH</span> Accelerating 
+          <br /> Technological Minds
         </h3>
         <p className="text-slate-500 text-center">Where Engineering students converge to improve their technological ingenuity and  <br className="md:block sm:hidden"/> enhance their research skills.</p>
         <Link href="/Sign-up" className="bg-white text-black font-semibold rounded p-1">
