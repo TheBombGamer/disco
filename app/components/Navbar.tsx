f75d79b344534b8db44c542db16a38c9f9fcb193
Emmanuel-xt <fattmane5@gmail.com>
2024-01-29 13:44:32 +0100

    jsx syntax error 3

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -74,7 +74,6 @@ const Navbar: React.FC = () => {
               (path.includes(link.href) && link.href.length > 1) ||
               path === link.href;
             return (
-              <>
               <li key={link.name} className="">
                 <Link href={link.href} className={``}>
                   <p
@@ -89,20 +88,7 @@ const Navbar: React.FC = () => {
                   </p>
                 </Link>
               </li>
-                <li key={link.name} className="">
-                  <Link href={link.href} className={``}>
-                    <p
-                      className={`${
-                        isActive
-                          ? "   transition-colors font-bold"
-                          : "   transition-colors text-gray-400"
-                      }`}
-                    >
-                      {link.name}
-                    </p>
-                  </Link>
-                </li>
-              </>
+        
 
             );
           })}
