15a1be28e97dcdac79b20e0291becbfe96bc116c
Emmanuel-xt <fattmane5@gmail.com>
2024-02-17 01:49:43 +0100

    Assignment Upload and fetch

---
+++ models/assignment.ts
@@ -0,0 +1,31 @@
+import { Schema, model, models } from 'mongoose';
+
+
+const assignmentSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, 'password is required!'],
+    },
+    pdf: {
+        type: String,
+        required: [true, 'password is required!'],
+    },
+    instruction: {
+        type: String,
+        required: [true, 'password is required!'],
+    },
+    course: {
+        type: String,
+        required: [true, 'password is required!'],
+    },
+    submissionDate: {
+        type: Date,
+        required: [true, 'password is required!'],
+    },
+},
+    { timestamps: true }
+);
+
+const Assignment = models.Assignment || model('Assignment', assignmentSchema);
+export default Assignment
+
