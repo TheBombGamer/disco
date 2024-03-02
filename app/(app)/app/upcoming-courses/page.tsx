c762cab1f413f359545be7e569ecde93fa94f86d
Emmanuel-xt <fattmane5@gmail.com>
2024-03-02 21:35:18 +0100

    updated

--- app/(app)/app/upcoming-courses/page.tsx
+++ app/(app)/app/upcoming-courses/page.tsx
@@ -1,12 +1,66 @@
-import React from 'react'
+"use client";
 
-const page = () => {
-  return (
-    <div>Upcoming Courses Would Appear Here
+import AssignmentCard from "@app/components/AssignmentCard";
+import UpcomingCard from "@app/components/UpcomingCard";
+import React, { useEffect, useState } from "react";
 
-    </div>
-
-  )
+interface Assignment {
+  _id: string;
+  title: string;
+  instruction: string;
+  pdf: string;
+  course: string;
+  submissionDate: string;
+  createdAt: string;
 }
 
-export default page
+const page: React.FC = () => {
+  const [assignments, setAssignments] = useState<Assignment[]>([]);
+  const [refresh, setRefresh] = useState(false);
+
+
+  useEffect(() => {
+    const fetchAssignments = async () => {
+      try {
+        const response = await fetch("/api/upcoming");
+        if (!response.ok) {
+          throw new Error("Failed to fetch assignments");
+        }
+        const data = await response.json();
+        setAssignments(data);
+      } catch (error) {
+        console.error("Error fetching assignments:", error);
+      }
+    };
+
+    fetchAssignments();
+  }, []);
+
+  return (
+    <div>
+      <h6 className="font-bold text-2xl mb-10">Upcoming Classes</h6>
+      {assignments.length > 0 ? (
+        <div className="flex flex-col gap-4">
+          {assignments.map((assignment) => (
+            <div key={assignment._id} className="flex flex-col gap-5">
+              <UpcomingCard
+                _id={assignment._id}
+                title={assignment.title}
+                instruction={assignment.instruction}
+                pdf={assignment.pdf}
+                course={assignment.course}
+                submissionDate={assignment.submissionDate}
+                createdAt={assignment.createdAt}
+                setRefresh={setRefresh}
+              />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <h5>No Class Update Found</h5>
+      )}
+    </div>
+  );
+};
+
+export default page;
